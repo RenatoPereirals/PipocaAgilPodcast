ebd5cc656269706fa0295e17a2ddf89026af0591
Orlando Pagnani <108426164+NetoPagnani@users.noreply.github.com>
2023-09-14 08:24:50 -0300

    apagandotudo

--- README.md
+++
@@ -1,2 +0,0 @@
-# Squad-Vermelho-Coral
- 
--- js/homescript.js
+++
@@ -1,8 +0,0 @@
-function assinar() {
-    window.location.href = "../html/cadastro.html";
-  }
-  function login() {
-    window.location.href = "../html/login.html";
-  }
-
-  

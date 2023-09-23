8bc582667c952f9f87d65d982f232beaaac83298
RenatoPereirals <renatopreirals@gmail.com>
2023-09-23 08:39:18 -0300

    Criação de testes de unidade para loginAttempService

--- src/app/services/login-attemp.service.spec.ts
+++ src/app/services/login-attemp.service.spec.ts
@@ -10,7 +10,32 @@ describe('LoginAttempService', () => {
     service = TestBed.inject(LoginAttempService);
   });
 
-  it('should be created', () => {
+  it('deve ser criado', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('deve registrar uma tentativa de login', () => {
+    const email = 'exemplo@exemplo.com';
+    service.recordLoginAttemp(email);
+    expect(service.isLoginBlocked(email)).toBeFalse();
+  });
+
+  it('deve verificar se o login está bloqueado após muitas tentativas', () => {
+    const email = 'exemplo@exemplo.com';
+    for (let i = 0; i < service.maxAttemps; i++) {
+      service.recordLoginAttemp(email);
+    }
+    expect(service.isLoginBlocked(email)).toBeTruthy();
+  });
+
+  it('deve redefinir as tentativas de login após uma redefinição', () => {
+    const email = 'exemplo@exemplo.com';
+    for (let i = 0; i < service.maxAttemps; i++) {
+      service.recordLoginAttemp(email);
+    }
+    expect(service.isLoginBlocked(email)).toBeTruthy();
+    service.resetLomginAttemps(email);
+
+    expect(service.isLoginBlocked(email)).toBeFalse();
   });
 });

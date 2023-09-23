998db9491beb9c7bd4478d84245f45a8690acfcc
RenatoPereirals <renatopreirals@gmail.com>
2023-09-23 08:54:18 -0300

    Criação de testes de unidade para jsonDataService

--- src/app/services/json-data.service.spec.ts
+++ src/app/services/json-data.service.spec.ts
@@ -18,35 +18,28 @@ describe('JsonDataService', () => {
   });
 
   beforeEach(() => {
-    // Obtenha o serviço e o HttpTestingController antes de cada teste
     service = TestBed.inject(JsonDataService);
     httpMock = TestBed.inject(HttpTestingController);
   });
 
   afterEach(() => {
-    // Verifique se todas as solicitações foram tratadas
     httpMock.verify();
   });
 
-  it('should be created', () => {
+  it('deve ser criado', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should make a GET request', () => {
-    const testData = {
+  it('deve fazer uma solicitação GET', () => {
+    const testData = {};
 
-    };
-
-    // Faça uma solicitação HTTP simulada
     service.getData().subscribe((data) => {
-      expect(data).toEqual(testData); // Verifica se os dados retornados são os esperados
+      expect(data).toEqual(testData);
     });
 
-    // Verifique a solicitação HTTP simulada
-    const req = httpMock.expectOne(service['jsonUrl']); // Observe que estamos acessando a propriedade privada jsonUrl do serviço para verificar a URL
-    expect(req.request.method).toBe('GET'); // Verifique se a solicitação é do tipo GET
+    const req = httpMock.expectOne(service['jsonUrl']);
+    expect(req.request.method).toBe('GET');
 
-    // Fornecer dados simulados como resposta à solicitação
     req.flush(testData);
   });
 });
